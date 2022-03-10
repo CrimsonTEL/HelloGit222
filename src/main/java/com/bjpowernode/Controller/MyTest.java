cd9ed0f5396206e479ba839f1efbe7cbedeecc51
LiYouJin <372499467@qq.com>
2022-03-10 17:37:21 +0800

    third commit

--- src/main/java/com/bjpowernode/Controller/MyTest.java
+++ src/main/java/com/bjpowernode/Controller/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("欢迎使用IDEA集成Git");
         System.out.println("欢迎使用IDEA集成Git222");
+        System.out.println("欢迎使用IDEA集成Git333");
     }
 }
